830eb1fbb86d5232e6a4aeb46051a02d78ed393a
DARKB100D <MATRIX.1.3@yandex.ru>
2019-06-16 17:28:45 +0300

    Add emoji, state, custom keyboards

--- wol.ino
+++ wol.ino
@@ -83,65 +83,109 @@ void loop() {
   }
 }
 
+int getState() {
+  if (is_sleep) return 1; // sleeping
+  if (digitalRead(power_led_in_pin)) return 3; // working
+  return 0; // soft-off
+}
+
 void sendStatus(String &chat_id) {
-  String keyboardJson = "[[ \"/on\", \"/off\"]]";
-  String state = "Сон";
-  if (!is_sleep) {
-    if (digitalRead(power_led_pin)) state = "Работает";
-    else state = "Выключен";
+  String state_msg = "Состояние ПК: ";
+  String msg = "\nДоступны действия:";
+  auto state = getState();
+  switch (state) {
+    case 0:
+      bot->sendMessageWithReplyKeyboard(chat_id, state_msg + state_work + msg, "", keyboardJson_work, true);
+      break;
+    case 1:
+      bot->sendMessageWithReplyKeyboard(chat_id, state_msg + state_sleep + msg, "", keyboardJson_sleep, true);
+      break;
+    default:
+      bot->sendMessageWithReplyKeyboard(chat_id, state_msg + state_off + msg, "", keyboardJson_off, true);
+      break;
   }
-  bot->sendMessageWithReplyKeyboard(chat_id, "Состояние: " + state, "", keyboardJson, true);
 }
 
 bool validateChat(String &chat_id) {
   if (chat_id == "") return false;
-  if (!chat_id.equals("350367633")) {
-    bot->sendMessage(chat_id, "401 Unauthorized.");
-    return false;
-  }
-  return true;
+  for (int i = 0; i < whiteList_length; i++) if (chat_id.equals(whiteList[i])) return true;
+  bot->sendMessage(chat_id, "401 Unauthorized.");
+  return false;
 }
 
 void power_sw(int duration) {
-    digitalWrite(power_sw_pin, LOW);
-    delay(duration);
-    digitalWrite(power_sw_pin, HIGH);
+  digitalWrite(power_sw_pin, HIGH);
+  delay(duration);
+  digitalWrite(power_sw_pin, LOW);
 }
 
 void reset_sw() {
-    power_sw(8000);
-    delay(500);
-    power_sw(200);
+  power_sw(8000);
+  delay(500);
+  power_sw(200);
+}
+
+String format2digit(int value) {
+  String result;
+  if (value < 10) result += "0";
+  result += String(value);
+  return result;
+}
+
+String getUptime() {
+  long secs = millis() / 1000; //convect milliseconds to seconds
+  long mins = secs / 60; //convert seconds to minutes
+  long hours = mins / 60; //convert minutes to hours
+  long days = hours / 24; //convert hours to days
+  secs = secs - (mins * 60); //subtract the coverted seconds to minutes in order to display 59 secs max
+  mins = mins - (hours * 60); //subtract the coverted minutes to hours in order to display 59 minutes max
+  hours = hours - (days * 24); //subtract the coverted hours to days in order to display 23 hours max
+  String uptime = "";
+  if (days > 0) uptime += String(days) + "d ";
+  uptime += format2digit(hours) + ":";
+  uptime += format2digit(mins) + ":";
+  uptime += format2digit(secs);
+  return uptime;
 }
 
 void executeCommand(String &text, String &chat_id) {
-  if (text.equals("/power_on")) {
+  Serial.println(text);
+  if (text.equals("/status") || text.equals("/start") 
+  || text.equals("u267b u041eu0431u043du043eu0432u0438u0442u044c") // обновить
+  || text.equals("ud83dudd19 u041du0430u0437u0430u0434")) { // Назад
+    sendStatus(chat_id);
+    return;
+  }
+  if (text.equals("/power_on") 
+  || text.equals("ud83cudd99 u0412u043au043bu044eu0447u0438u0442u044c") // включить
+  || text.equals("ud83cudd99 u0420u0430u0437u0431u0443u0434u0438u0442u044c")) { // разбудить
     power_sw(200);
+    sendStatus(chat_id);
     return;
   }
-  if (text.equals("/power_off")) {
+  if (text.equals("/power_off") || text.equals("ud83dudd34 u0412u044bu043au043bu044eu0447u0438u0442u044c")) {
     power_sw(8000);
+    sendStatus(chat_id);
     return;
   }
-  if (text.equals("/hard_reset")) {
+  if (text.equals("/hard_reset") || text.equals("ud83dudd04 u041fu0435u0440u0435u0437u0430u0433u0440u0443u0437u0438u0442u044c")) {
     reset_sw();
+    sendStatus(chat_id);
     return;
   }
-  if (text.equals("/good_night_led_on")) {
+  if (text.equals("/good_night") || text.equals("ud83dudd05 u0418u043du0434u0438u043au0430u0442u043eu0440u044b")) {
+    digitalWrite(good_night_control_pin, !digitalRead(good_night_control_pin));
     return;
   }
-  if (text.equals("/good_night_led_off")) {
+  if (text.equals("/status_esp") || text.equals("u26ab ESP")) {
+    String values = "SSID: " + WiFi.SSID() + "\n";
+    values += "IP: " + WiFi.localIP().toString() + "\n";
+    values += "Uptime: " + getUptime();
+    bot->sendMessage(chat_id, values);
     return;
   }
-  if (text.equals("/status")) {
-    return;
-  }
-  if (text.equals("/start")) {
-    String start_msg = "/power_on - Включить (разбудить)\n"
-    "/power_off - Жесткое выключение\n"
-    "/hard_reset - Жесткая перезагрузка\n"
-    "/status - Теущее состояние ПК\n";
-    bot->sendMessage(chat_id, start_msg);
+  if (text.equals("u2728 u0415u0449u0451")) { // Ещё
+    bot->sendMessageWithReplyKeyboard(chat_id, "Дополнительно:", "", keyboardJson_menu, true);
     return;
   }
   bot->sendMessage(chat_id, "404 Not Found.");
@@ -152,6 +196,5 @@ void handleNewMessages(int &numNewMessages) {
     String chat_id = String(bot->messages[i].chat_id);
     if (!validateChat(chat_id)) continue;
     executeCommand(bot->messages[i].text, chat_id);
-    sendStatus(chat_id);
   }
 }
